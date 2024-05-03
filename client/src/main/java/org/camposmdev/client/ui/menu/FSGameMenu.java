5a64f282578989e876f34b6034bb240fd14e5f7e
CamposmDev <camposm.dev@gmail.com>
2024-05-02 23:59:45 -0400

    Implemented How to Play Menu Teaches player how the games works Added some documentation

--- client/src/main/java/org/camposmdev/client/ui/menu/FSGameMenu.java
+++ client/src/main/java/org/camposmdev/client/ui/menu/FSGameMenu.java
@@ -4,6 +4,7 @@ import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.MenuType;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.texture.Texture;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
@@ -16,9 +17,11 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.scene.web.WebView;
 import javafx.util.Duration;
 import org.camposmdev.client.entity.sprite.SecretsSpriteAtlas;
 import org.camposmdev.util.FXUtil;
+import org.camposmdev.util.Log;
 import org.jetbrains.annotations.NotNull;
 
 import static com.almasb.fxgl.dsl.FXGL.*;
@@ -30,6 +33,7 @@ public class FSGameMenu extends FXGLMenu {
     private VBox optionBox;
     private CheckBox cbFullScreen;
     private Slider sfxSlider, musicSlider;
+    private WebView booklet;
 
     public FSGameMenu(@NotNull MenuType type) {
         super(type);
@@ -39,6 +43,11 @@ public class FSGameMenu extends FXGLMenu {
         initOptionBox();
         /* init menu box */
         initMenuBox();
+        /* init booklet */
+        Platform.runLater(() -> {
+            booklet = FXUtil.loadBooklet();
+            booklet.setPrefSize(getAppWidth(), getAppHeight());
+        });
         /* init root container */
         root = new StackPane(background, menuBox);
         root.setPrefWidth(getAppWidth());
@@ -46,6 +55,29 @@ public class FSGameMenu extends FXGLMenu {
         root.setTranslateY(getAppHeight());
         addChild(root);
     }
+
+	@Override
+	public void onCreate() {
+		/* set the current state if the game is full screen */
+		cbFullScreen.setSelected(getSettings().getFullScreen().getValue());
+		/* set the current volume for sfxSlider */
+		sfxSlider.setValue(getSettings().getGlobalSoundVolume());
+		/* set the current volume for musicSlider */
+		musicSlider.setValue(getSettings().getGlobalMusicVolume());
+		/* translate the menu */
+		FXUtil.animation().translate(root).to(new Point2D(0,0)).duration(Duration.millis(200)).build().play();
+		play("feedback/book page turn.wav");
+	}
+
+	@Override
+	public void onDestroy() {
+		/* return to the menu box */
+		if (!root.getChildren().contains(menuBox)) {
+			root.getChildren().setAll(initBackground(), menuBox);
+		}
+		removeChild(booklet);
+		root.setTranslateY(getAppHeight());
+	}
 
     private void initOptionBox() {
         /* init full screen control */
@@ -76,21 +108,37 @@ public class FSGameMenu extends FXGLMenu {
         optionBox.setAlignment(Pos.CENTER);
     }
 
-    private void initMenuBox() {
+	/**
+	 * Initializes the main menu that displays when user pauses the game.
+	 */
+	private void initMenuBox() {
         /* init options menu */
-        var bt0 = createButton("Options", e -> {
+        var btOptions = createButton("Options", e -> {
             root.getChildren().remove(menuBox);
             root.getChildren().add(optionBox);
         });
+        var btControls = createButton("Controls", e -> {
+            Log.info("Implement me!");
+            /* TODO - Implement controls menu if any controls available for the player to play the game
+            *   besides point and click. Maybe key binds for shortcuts with the user interface
+            *   like in RTS games. Make their gameplay more efficient. */
+        });
+        var btBook = createButton("How to Play", e -> {
+            addChild(booklet);
+        });
         /* init resume menu */
-        var bt1 = createButton("Resume", e -> this.fireResume());
+        var btResume = createButton("Resume", e -> this.fireResume());
         /* init exit game menu */
-        var bt2 = createButton("Exit Game", e -> this.fireExitToMainMenu());
-        menuBox = new VBox((BLOCK_MARGIN), bt0, bt1, bt2);
+        var btExit = createButton("Exit Game", e -> this.fireExitToMainMenu());
+        menuBox = new VBox((BLOCK_MARGIN), btResume, btBook, btControls, btOptions, btExit);
         menuBox.setAlignment(Pos.CENTER);
     }
 
-    private Texture initBackground() {
+	/**
+	 * Initializes the background for the main menu.
+	 * @return texture of game board
+	 */
+	private Texture initBackground() {
         var texture = FXGL.texture("spritesheets/secrets.png");
         SecretsSpriteAtlas atlas = FXUtil.loadJSON("spritesheets/secrets.json", SecretsSpriteAtlas.class);
         assert atlas != null;
@@ -101,42 +149,35 @@ public class FSGameMenu extends FXGLMenu {
         return background;
     }
 
-    private Text createButton(String content, EventHandler<MouseEvent> event) {
-        Text node = getUIFactoryService().newText(content, Color.BLACK, 32);
-        node.setTextAlignment(TextAlignment.CENTER);
-        if (event == null) return node;
-        node.setOnMouseClicked(event);
-        node.setOnMouseEntered(e -> {
-            node.setScaleX(1.25);
-            node.setScaleY(1.25);
-        });
-        node.setOnMouseExited(e -> {
-            node.setScaleX(1);
-            node.setScaleY(1);
-        });
-        return node;
-    }
+	/**
+	 * Utility function to build a button with custom styling.
+	 * @param content
+	 * @param event
+	 * @return
+	 */
+	private Text createButton(String content, EventHandler<MouseEvent> event) {
+		Text node = getUIFactoryService().newText(content, Color.BLACK, 32);
+		node.setTextAlignment(TextAlignment.CENTER);
+		if (event == null) return node;
+		node.setOnMouseClicked(event);
+		node.setOnMouseEntered(e -> {
+			node.setScaleX(1.25);
+			node.setScaleY(1.25);
+		});
+		node.setOnMouseExited(e -> {
+			node.setScaleX(1);
+			node.setScaleY(1);
+		});
+		return node;
+	}
 
-    private Text createText(String content) {
-        return createButton(content, null);
-    }
+	/**
+	 * Utility function to build text with custom styling.
+	 * @param content
+	 * @return
+	 */
+	private Text createText(String content) {
+		return createButton(content, null);
+	}
 
-    @Override
-    public void onCreate() {
-        cbFullScreen.setSelected(getSettings().getFullScreen().getValue());
-        sfxSlider.setValue(getSettings().getGlobalSoundVolume());
-        musicSlider.setValue(getSettings().getGlobalMusicVolume());
-        FXUtil.animation().translate(root).to(new Point2D(0,0)).duration(Duration.millis(200)).build().play();
-        play("feedback/book page turn.wav");
-    }
-
-    @Override
-    public void onDestroy() {
-        /* return to the menu box */
-        if (!root.getChildren().contains(menuBox)) {
-            root.getChildren().setAll(initBackground(), menuBox);
-        }
-        root.setTranslateY(getAppHeight());
-//        FXUtil.animation().translate(root).to(new Point2D(0,getAppHeight())).duration(Duration.millis(200)).build().play();
-    }
 }
