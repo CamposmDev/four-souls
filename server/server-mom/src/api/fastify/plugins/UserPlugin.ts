fabb7e63882fd942c76fa317caac9b5fe4e8a26e
CamposmDev <camposm.dev@gmail.com>
2024-06-01 20:58:13 -0400

    Updated server-mom module Updated endpoint /api/user/:id to no longer need authentication

--- server/server-mom/src/api/fastify/plugins/UserPlugin.ts
+++ server/server-mom/src/api/fastify/plugins/UserPlugin.ts
@@ -4,7 +4,7 @@ import { auth, checker } from "../hooks";
 
 const UserPlugin: FastifyPluginAsync = async (app: FastifyInstance) => {
     app.post("/", { preHandler: checker.user.create }, UserController.create)
-    app.get("/:id", { onRequest: auth.verifyJWT,  preHandler: checker.user.getById }, UserController.getById)
+    app.get("/:id", { preHandler: checker.user.getById }, UserController.getById)
     app.post("/login", { preHandler: checker.user.login }, UserController.login)
     app.post("/logout", { preHandler: auth.verifyJWT }, UserController.logout)
 }
