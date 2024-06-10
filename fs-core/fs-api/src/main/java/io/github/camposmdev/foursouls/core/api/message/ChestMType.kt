7da15ef3c4543be5de840ccdf4d7f13a32b1f4ad
CamposmDev <camposm.dev@gmail.com>
2024-06-09 23:59:27 -0400

    docs(fs-api): Add documentation to ChestMType enum
    Added detailed documentation to the ChestMType enum in the fs-api module.
    This improves code readability and helps future developers understand
    the purpose and usage of each enum value.

--- fs-core/fs-api/src/main/java/io/github/camposmdev/foursouls/core/api/message/ChestMType.kt
+++ fs-core/fs-api/src/main/java/io/github/camposmdev/foursouls/core/api/message/ChestMType.kt
@@ -1,11 +1,18 @@
 package io.github.camposmdev.foursouls.core.api.message
 
 enum class ChestMType {
-    GREETING,
-    CHAT,
-    DONE,
-    USERS,
-    CLOSED,
-    ERROR,
-    LEAVE;
+    GREETING, /* initial message sent to user after verification/joining server. */
+    CHAT, /* sent to client and server. Server relays message to other users. */
+    DONE, /* sent to server by host. Notifies host is done with the server and wishes to close.   */
+    USERS, /* sent to users when a new user joins the server. */
+    CLOSED, /* sent to users when the host closes the server or an unexpected error occurs in the system. */
+    ERROR, /* sent to user whenever an exception occurs. Exceptions can be but not limited to invalid messages or invalid JSONs (decode errors). */
+    LEAVE, /* sent to server when the sender (user) wants to leave the server. Server unregisters the user and notifies other users the sender (user) has left the server. If the host is the sender then the host sends a http request to mom to unlock the server and then the server kicks everyone out. */
+    LOOT_DRAW,
+    LOOT,
+    END_TURN,
+    CHARACTER_DRAW,
+    CHARACTERS,
+    CHARACTER_CHOICE,
+
 }
